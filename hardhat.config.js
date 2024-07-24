6f3488b101760c4dd7dda30eb7441a5180e60447
giratine <hiepnguyennamlong@gmail.com>
2024-07-24 14:07:07 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-const PRIVATE_KEY = "434baadec3278e33fe33b98473c3ad126134292cfe430b12afc44846c43f6773";
+const PRIVATE_KEY = "";
 
 module.exports = {
   solidity: "0.8.19",
